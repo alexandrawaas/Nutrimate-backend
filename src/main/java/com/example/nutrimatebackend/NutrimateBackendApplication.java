6bb70c8965368679f2f0ffc4a7297707f4561eba
awaas <alexandra.waas@hof-university.de>
2024-04-15 10:59:25 +0200

    Created spring boot project and swagger docs

---
+++ src/main/java/com/example/nutrimatebackend/NutrimateBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.nutrimatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimateBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NutrimateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nutrimatebackend/NutrimateBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.nutrimatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimateBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
