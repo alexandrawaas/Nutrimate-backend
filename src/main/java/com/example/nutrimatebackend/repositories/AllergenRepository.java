f0a2d35318e22a788ba6321c105e01d44f684d21
Tim <timwagner997@gmail.com>
2024-05-03 10:19:43 +0200

    implemented repositories

--- src/main/java/com/example/nutrimatebackend/repositories/AllergenRepository.java
+++ src/main/java/com/example/nutrimatebackend/repositories/AllergenRepository.java
@@ -1,4 +1,10 @@
 package com.example.nutrimatebackend.repositories;
 
-public class AllergenRepository {
+import com.example.nutrimatebackend.entities.Allergen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AllergenRepository extends JpaRepository<Allergen, Long>, JpaSpecificationExecutor<Allergen> {
 }
--- src/main/java/com/example/nutrimatebackend/repositories/FoodRepository.java
+++ src/main/java/com/example/nutrimatebackend/repositories/FoodRepository.java
@@ -1,4 +1,10 @@
 package com.example.nutrimatebackend.repositories;
 
-public class FoodRepository {
+import com.example.nutrimatebackend.entities.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long>, JpaSpecificationExecutor<Food> {
 }
