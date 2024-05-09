3f31e05c2dd8d595f72ad80a061a2ca7093444d8
Tim <timwagner997@gmail.com>
2024-05-09 22:30:24 +0200

    added endpoint for calculation the environmental score for a users fridge

--- src/main/java/com/example/nutrimatebackend/controllers/EnvironmentalScoreController.java
+++ src/main/java/com/example/nutrimatebackend/controllers/EnvironmentalScoreController.java
@@ -1,7 +1,38 @@
 package com.example.nutrimatebackend.controllers;
 
+import com.example.nutrimatebackend.entities.Food;
+import com.example.nutrimatebackend.repositories.FoodRepository;
+import com.example.nutrimatebackend.repositories.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EnvironmentalScoreController {
+    private final UserRepository userRepository;
+
+    public EnvironmentalScoreController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = "/environmental-score")
+    public int getEnvironmentalScore()
+    {
+        Long currentUser = 1L;
+
+        int environmentalScore = 0;
+
+        List<Food> allFoods = userRepository
+                .findById(currentUser)
+                .orElseThrow()
+                .getFridge()
+                .getContent();
+
+        for (Food food : allFoods) {
+            environmentalScore += food.calculateEnvironmentalScore();
+        }
+
+        return environmentalScore / allFoods.size();
+    }
 }
