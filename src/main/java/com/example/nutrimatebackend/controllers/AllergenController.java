1faeb496ff72ae82dbc2588a0fd9cdc45f1e61de
DESKTOP-9RDOD23\Hien Le <dang.le@hof-university.de>
2024-05-10 10:21:49 +0200

    implemented AllergenController

--- src/main/java/com/example/nutrimatebackend/controllers/AllergenController.java
+++ src/main/java/com/example/nutrimatebackend/controllers/AllergenController.java
@@ -1,8 +1,24 @@
 package com.example.nutrimatebackend.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.nutrimatebackend.entities.Allergen;
+import com.example.nutrimatebackend.repositories.AllergenRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AllergenController {
-    // TODO: Hien (GET für die Allergene)
+    private final AllergenRepository allergenRepository;
+
+    public AllergenController(AllergenRepository allergenRepository) {
+        this.allergenRepository = allergenRepository;
+    }
+
+    @GetMapping(value = "/allergens")
+    public List<Allergen> getAllAllergens() {
+        return allergenRepository.findAll();
+    }
+
 }
