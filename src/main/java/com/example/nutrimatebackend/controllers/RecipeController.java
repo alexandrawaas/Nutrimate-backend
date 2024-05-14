7eadc241f35963352b7ddee95c0ad3ae9cf26dc4
Tim <timwagner997@gmail.com>
2024-05-14 22:54:52 +0200

    call edamam api to fetch recipes

--- src/main/java/com/example/nutrimatebackend/controllers/RecipeController.java
+++ src/main/java/com/example/nutrimatebackend/controllers/RecipeController.java
@@ -1,8 +1,10 @@
 package com.example.nutrimatebackend.controllers;
 
+import com.example.nutrimatebackend.dtos.api.EdamamRawRecipeResponse;
 import com.example.nutrimatebackend.dtos.recipe.RecipeDTOResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,23 @@ public class RecipeController {
     public List<RecipeDTOResponse> getRecipes() {
         // TODO: fetch recipes suitable for the users fridges content
 
-        List<RecipeDTOResponse> dummyRecipes = new ArrayList<>();
+        RestTemplate restTemplate = new RestTemplate();
 
-        dummyRecipes.add(new RecipeDTOResponse("https://www.edamam.com/results/recipe/?recipe=coke-to-float-your-boat-f9d74107de917f6c36431c2fa079670a"));
-        dummyRecipes.add(new RecipeDTOResponse("https://www.edamam.com/results/recipe/?recipe=mcdonald%27s-mcgriddle-73ba319ca84fb238617f035d56718323"));
-        dummyRecipes.add(new RecipeDTOResponse("https://www.edamam.com/results/recipe/?recipe=spicy-sausage-pizza-ebdd6b4b4f3c4fb3a231866a25992ee3"));
+        String url = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=c0f268ea&app_key=ae91ed6e3daff98e50ae16324d419b63";
 
-        return dummyRecipes;
+        EdamamRawRecipeResponse response = restTemplate.getForObject(url, EdamamRawRecipeResponse.class);
+
+        List<RecipeDTOResponse> recipeURLs = new ArrayList<>();
+
+        for (EdamamRawRecipeResponse.Hit hit : response.getHits()) {
+            recipeURLs.add(new RecipeDTOResponse(
+                    hit
+                            .get_links()
+                            .getSelf()
+                            .getHref())
+            );
+        }
+
+        return recipeURLs;
     }
 }
