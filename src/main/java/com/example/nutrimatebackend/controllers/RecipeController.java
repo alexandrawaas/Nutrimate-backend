443d6f0e57d34807ca053057b1b62ecc31556df9
Tim <timwagner997@gmail.com>
2024-05-23 09:43:07 +0200

    moved recipe search logic to RecipeService

--- src/main/java/com/example/nutrimatebackend/controllers/RecipeController.java
+++ src/main/java/com/example/nutrimatebackend/controllers/RecipeController.java
@@ -1,64 +1,22 @@
 package com.example.nutrimatebackend.controllers;
 
-import com.example.nutrimatebackend.dtos.api.EdamamRawRecipeResponse;
 import com.example.nutrimatebackend.dtos.recipe.RecipeDTOResponse;
-import org.apache.http.client.utils.URIBuilder;
+import com.example.nutrimatebackend.services.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class RecipeController {
-    private WebClient webClient;
+    private final RecipeService recipeService;
 
-    public RecipeController(WebClient webClient) {
-        this.webClient = webClient;
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @GetMapping("/recipes")
     public List<RecipeDTOResponse> getRecipes() {
-        // TODO: fetch recipes suitable for the users fridges content
-
-        String url = new URIBuilder()
-                .setScheme("https")
-                .setHost("api.edamam.com")
-                .setPath("/api/recipes/v2")
-                .addParameter("type", "public")
-                .addParameter("app_id", "c0f268ea")
-                .addParameter("app_key", "ae91ed6e3daff98e50ae16324d419b63")
-
-                // Search recipes with "chicken"
-                .addParameter("q", "chicken")
-
-                .toString();
-
-        EdamamRawRecipeResponse response = webClient
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(EdamamRawRecipeResponse.class)
-                .block();
-
-        List<RecipeDTOResponse> recipeURLs = new ArrayList<>();
-
-        if (response == null) {
-            return Collections.emptyList();
-        }
-
-        for (EdamamRawRecipeResponse.Hit hit : response.getHits()) {
-            String recipeURL = hit
-                    .get_links()
-                    .getSelf()
-                    .getHref();
-
-            RecipeDTOResponse recipeDTOResponse = new RecipeDTOResponse(recipeURL);
-            recipeURLs.add(recipeDTOResponse);
-        }
-
-        return recipeURLs;
+        return recipeService.searchRecipes();
     }
 }
--- src/main/java/com/example/nutrimatebackend/services/RecipeService.java
+++ src/main/java/com/example/nutrimatebackend/services/RecipeService.java
@@ -1,9 +1,63 @@
 package com.example.nutrimatebackend.services;
 
+import com.example.nutrimatebackend.dtos.api.EdamamRawRecipeResponse;
+import com.example.nutrimatebackend.dtos.recipe.RecipeDTOResponse;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class RecipeService
 {
+    private final WebClient webClient;
 
+    public RecipeService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<RecipeDTOResponse> searchRecipes() {
+        // TODO: fetch recipes suitable for the users fridges content
+
+        String url = new URIBuilder()
+                .setScheme("https")
+                .setHost("api.edamam.com")
+                .setPath("/api/recipes/v2")
+                .addParameter("type", "public")
+                .addParameter("app_id", "c0f268ea")
+                .addParameter("app_key", "ae91ed6e3daff98e50ae16324d419b63")
+
+                // Search recipes with "chicken"
+                .addParameter("q", "chicken")
+
+                .toString();
+
+        EdamamRawRecipeResponse response = webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(EdamamRawRecipeResponse.class)
+                .block();
+
+        List<RecipeDTOResponse> recipeURLs = new ArrayList<>();
+
+        if (response == null) {
+            return Collections.emptyList();
+        }
+
+        for (EdamamRawRecipeResponse.Hit hit : response.getHits()) {
+            String recipeURL = hit
+                    .get_links()
+                    .getSelf()
+                    .getHref();
+
+            RecipeDTOResponse recipeDTOResponse = new RecipeDTOResponse(recipeURL);
+            recipeURLs.add(recipeDTOResponse);
+        }
+
+        return recipeURLs;
+    }
 }
