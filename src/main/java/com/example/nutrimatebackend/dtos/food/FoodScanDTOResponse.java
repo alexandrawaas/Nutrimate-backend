361e0a330e8a0d4bf2eb3345732d93ef87b9d1fb
Tim <timwagner997@gmail.com>
2024-05-29 14:59:19 +0200

    added required properties to FoodScanDTOResponse

--- src/main/java/com/example/nutrimatebackend/dtos/food/FoodScanDTOResponse.java
+++ src/main/java/com/example/nutrimatebackend/dtos/food/FoodScanDTOResponse.java
@@ -1,17 +1,36 @@
 package com.example.nutrimatebackend.dtos.food;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+import java.util.List;
+
+@Data
 public class FoodScanDTOResponse {
-
-
-
-
-
-
     private String barcode;
+    private double calories;
+    private double fat;
+    private double saturatedFats;
+    private double carbs;
+    private double sugar;
+    private double fibers;
+    private double protein;
+    private double salt;
 
-    public FoodScanDTOResponse(String barcode) {
+    private List<String> allergens;
+
+    private String category;
+
+    public FoodScanDTOResponse(String barcode, double calories, double fat, double saturatedFats, double carbs, double sugar, double fibers, double protein, double salt, List<String> allergens, String category) {
         this.barcode = barcode;
+        this.calories = calories;
+        this.fat = fat;
+        this.saturatedFats = saturatedFats;
+        this.carbs = carbs;
+        this.sugar = sugar;
+        this.fibers = fibers;
+        this.protein = protein;
+        this.salt = salt;
+        this.allergens = allergens;
+        this.category = category;
     }
 }
