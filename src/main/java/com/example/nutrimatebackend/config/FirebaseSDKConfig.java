29d87b0fe50fef8d1db58350138954ddf7c029bf
Tim <timwagner997@gmail.com>
2024-06-23 02:48:33 +0200

    refactor + rename

--- src/main/java/com/example/nutrimatebackend/config/FirebaseSDKConfig.java
+++ src/main/java/com/example/nutrimatebackend/config/FirebaseSDKConfig.java
@@ -5,17 +5,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class GoogleSecurityConfig {
+public class FirebaseSDKConfig {
     @Bean
     public FirebaseApp initFirebaseSDK() throws IOException {
         FileInputStream serviceAccount =
@@ -26,22 +23,5 @@ public class GoogleSecurityConfig {
                 .build();
 
         return FirebaseApp.initializeApp(options);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web
-                .ignoring()
-                .requestMatchers("/login");
     }
 }
